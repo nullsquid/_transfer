f50fb340d1c8c7afe035d8c7cf5eb76c9fb5c0d9
Azrael <j.shepard.ramsay@gmail.com>
2015-03-26 21:16:23 -0400

    added xml parser class

---
+++ Assets/Scripts/XMLParser.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Xml;
+
+public class XMLParser : MonoBehaviour {
+
+	public XmlDocument document = new XmlDocument();
+	// Use this for initialization
+	void Start () {
+		//document.Load("Resources/1A.xml");
+		//Resources.Load("1A.xml");
+		document.Load("Assets/Resources/1A.xml");
+		XmlElement firstChild = (XmlElement)document.DocumentElement.FirstChild;
+		print(firstChild);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
