c43a0927f00aa8c9a06e9b9487d91b8c61bbb8c5
Hyacinth <j.shepard.ramsay@gmail.com>
2015-04-01 00:50:07 -0400

    set up dir variables

--- Assets/Directory.cs
+++ Assets/Directory.cs
@@ -1,8 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using System.Collections.Generic;
 public class Directory : MonoBehaviour {
 
+	public string path;
+	public List<string> files;
 	// Use this for initialization
 	void Start () {
 	
