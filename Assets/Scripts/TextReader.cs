397923bc068ecb1d60dda7d6a1a9caf17eae7e6a
Azrael <j.shepard.ramsay@gmail.com>
2015-04-11 15:03:34 -0400

    got json parser working; added textassets

---
+++ Assets/Scripts/TextReader.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using SimpleJSON;
+//using JsonDotNet;
+
+public class TextReader : MonoBehaviour {
+	//public TextAsset text;
+	//JSON N = JSON.Parse(
+	public TextAsset text; //= Resources.Load("JSONTEST") as TextAsset; //= //Resources.Load("JSONTest") as TextAsset;
+	public TextAsset _1A;
+	public TextAsset _1B;
+	public TextAsset _1C;
+	public TextAsset _1D;
+	public TextAsset _1E;
+	public TextAsset _1F;
+
+	//string N = JSON.Parse(text.text);
+	//string N = JSON.Parse(
+	// Use this for initialization
+	void Start(){
+		var N = JSONNode.Parse(text.text);
+		//string N = JSON.Parse(text.text);
+		//Debug.Log(N);
+		//int version = N["version"].Value;
+		Debug.Log(N["version"].Value);
+		//Parse()
+
+	}
+	void Update(){
+
+	}
+	void Parse(string text){
+
+	}
+}
