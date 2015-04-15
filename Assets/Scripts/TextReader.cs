7b223485b44aa7fd8bd206d3bfa2e70b7497b0fc
Azrael <j.shepard.ramsay@gmail.com>
2015-04-14 22:01:04 -0400

    integrated simplejson into existing code

--- Assets/Scripts/TextReader.cs
+++ Assets/Scripts/TextReader.cs
@@ -17,21 +17,25 @@ public class TextReader : MonoBehaviour {
 	public TextAsset _9A;
 	string W;
 	string[] responses = new string[2];
-
+	string val;
 	//public string[] test = new string[2];
 
 	//string N = JSON.Parse(text.text);
 	//string N = JSON.Parse(
 	// Use this for initialization
 	void Start(){
-		var N = JSONNode.Parse(text.text);
+		var N = JSONNode.Parse(_9A.text);
+
+		val = N["C"]["2"]["responses"][1];
 		//string N = JSON.Parse(text.text);
 		//Debug.Log(N);
 		//int version = N["version"].Value;
-		Debug.Log(N["version"].Value);
+		//Debug.Log(N["version"].Value);
+		Debug.Log(N["C"]["2"]["prompt"]);
+		Debug.Log(val);
 		//Parse()
 		//var W = JSONNode.Parse(_9A.text);
-		W = JSONNode.Parse(_9A.text);
+		//W = JSONNode.Parse(_9A.text);
 		//responses[0] = W["C"]["2"]["responses"];
 
 	}
