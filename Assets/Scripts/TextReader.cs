fdc22208e6bbf22c3d2a5b7c2c96140782955f5b
Azrael <j.shepard.ramsay@gmail.com>
2015-04-22 09:10:20 -0400

    added lists to hold json files for each level

--- Assets/Scripts/TextReader.cs
+++ Assets/Scripts/TextReader.cs
@@ -1,20 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using SimpleJSON;
 
 
 public class TextReader : MonoBehaviour {
-	//public TextAsset text;
-	//JSON N = JSON.Parse(
-	public TextAsset text; //= Resources.Load("JSONTEST") as TextAsset; //= //Resources.Load("JSONTest") as TextAsset;
-	public TextAsset _1A;
-	public TextAsset _1B;
-	public TextAsset _1C;
-	public TextAsset _1D;
-	public TextAsset _1E;
-	public TextAsset _1F;
 
-	public TextAsset _9A;
+	public List<TextAsset> _9 = new List<TextAsset>();
+	public List<TextAsset> _8 = new List<TextAsset>();
+	public List<TextAsset> _7 = new List<TextAsset>();
+	public List<TextAsset> _6 = new List<TextAsset>();
+	public List<TextAsset> _5 = new List<TextAsset>();
+	public List<TextAsset> _4 = new List<TextAsset>();
+	public List<TextAsset> _3 = new List<TextAsset>();
+	public List<TextAsset> _2 = new List<TextAsset>();
+	public List<TextAsset> _1 = new List<TextAsset>();
+
 	int blockNumber = 0;
 	string W;
 	string[] responses = new string[2];
@@ -25,16 +26,18 @@ public class TextReader : MonoBehaviour {
 	//string N = JSON.Parse(
 	// Use this for initialization
 	void Start(){
-		var N = JSONNode.Parse(_9A.text);
-
-		val = N["C"]["2"]["responses"][1];
+		var _9A = JSONNode.Parse(_9[0].text);
+		Debug.Log(_9A["C"]["1"]["prompt"]);
+		//var N = JSONNode.Parse(_9A.text);
+		var W = JSONNode.Parse(_9[0].text);
+		//val = N["C"]["2"]["responses"][1];
 		//string N = JSON.Parse(text.text);
 		//Debug.Log(N);
 		//int version = N["version"].Value;
 		//Debug.Log(N["version"].Value);
-		Debug.Log(N["C"]["1"]["prompt"]);
-		Debug.Log(N["C"]["2"]["prompt"]);
-		Debug.Log(val);
+		//Debug.Log(N["C"]["1"]["prompt"]);
+		//Debug.Log(N["C"]["2"]["prompt"]);
+		//Debug.Log(val);
 		//Parse()
 		//var W = JSONNode.Parse(_9A.text);
 		//W = JSONNode.Parse(_9A.text);
