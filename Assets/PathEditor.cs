15badaf8dda15c8349147ec9ab34890798c473c0
Hyacinth <j.shepard.ramsay@gmail.com>
2015-06-07 08:57:24 -0400

    added lists to manage the files used for each character path

--- Assets/PathEditor.cs
+++ Assets/PathEditor.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public enum CharacterToEdit{
 	A,
@@ -14,15 +15,25 @@ public enum CharacterToEdit{
 	Z
 };
 
+
 public class PathEditor : MonoBehaviour {
 	public CharacterToEdit dropdown = CharacterToEdit.A;
+	public List<string> FilesForA = new List<string>();
+	public List<string> FilesForB = new List<string>();
+	public List<string> FilesForC = new List<string>();
+	public List<string> FilesForD = new List<string>();
+	public List<string> FilesForE = new List<string>();
+	public List<string> FilesForF = new List<string>();
+	public List<string> FilesForG = new List<string>();
+	public List<string> FilesForH = new List<string>();
+	public List<string> FilesForI = new List<string>();
+	public List<string> FilesFor0 = new List<string>();
+
 	// Use this for initialization
 	void Start () {
 
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
-	}
+
 }
