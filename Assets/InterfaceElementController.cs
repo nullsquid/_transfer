939081137ec66e3cc992fa57cac99ba80e375130
Azrael <j.shepard.ramsay@gmail.com>
2015-07-24 19:57:46 -0400

    made header disappear when connection interface appears

--- Assets/InterfaceElementController.cs
+++ Assets/InterfaceElementController.cs
@@ -2,15 +2,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.Events;
+using UnityEngine.UI;
+
 public class InterfaceElementController : MonoBehaviour {
 
 	public GameObject connectionInterface;
-
+	public Text header;
 
 	bool connectionInterfaceExists = false;
 
+	#region eventlisteners
 	private UnityAction connectListener;
 	private UnityAction exitConnectListener;
+	#endregion
 
 	public GameObject newConnectionInterface;
 
@@ -35,6 +39,7 @@ public class InterfaceElementController : MonoBehaviour {
 		if(connectionInterfaceExists == false){
 			newConnectionInterface = Instantiate(connectionInterface, transform.position = Vector3.zero, transform.rotation) as GameObject;
 			newConnectionInterface.transform.parent = transform;
+			header.enabled = false;
 			Debug.Log("connection loaded");
 		}
 		connectionInterfaceExists = true;
@@ -44,6 +49,8 @@ public class InterfaceElementController : MonoBehaviour {
 		if(connectionInterfaceExists == true){
 			Destroy(newConnectionInterface);
 			newConnectionInterface = null;
+			header.enabled = true;
+
 		}
 		connectionInterfaceExists = false;
 		Debug.Log("exit");
