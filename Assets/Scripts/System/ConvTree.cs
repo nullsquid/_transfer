dd58f72202c5286d371cdb25fa2b496baf1997c7
Hyacinth <j.shepard.ramsay@gmail.com>
2015-07-29 23:59:44 -0400

    began writing node traversal algorithm

--- Assets/Scripts/System/ConvTree.cs
+++ Assets/Scripts/System/ConvTree.cs
@@ -5,10 +5,14 @@ using System.Collections.Generic;
 public class ConvTree : MonoBehaviour {
 
 	public PathEditor pathEditor;
+	public ConvNode curNode;
+	public int curLevel;
+
 	public List<Transform> branches = new List<Transform>();
 	private Transform[] children;
 	// Use this for initialization
 	void Start () {
+		curLevel = 0;
 		BuildTree();
 		//foreach(Transform child in transform){
 		//	branches.Add(child);
---
+++ Assets/TreeTraversal.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TreeTraversal : MonoBehaviour {
+	//HACK
+	//temporary until we can get the tree automatically
+	public ConvTree curTree;
+
+
+	//some way of getting the current conversation tree
+	/*
+	 * 
+	*/
+	public int ChooseNode(ConvTree tree, int choice){
+
+		return 0;
+	}
+	public ConvNode FindNewNode(ConvTree tree){
+
+		return null;
+	}
+
+	public ConvNode NodeChange(ConvTree tree){
+
+		return null;
+	}
+
+	//public
+
+}
