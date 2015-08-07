f51d399f7b17f7ee1918df2448d4cedd11aa6d95
Azrael <j.shepard.ramsay@gmail.com>
2015-08-06 20:41:35 -0400

    wrote method for replacing names and pronouns

--- Assets/TextExtractAndDisplay.cs
+++ Assets/TextExtractAndDisplay.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 public class TextExtractAndDisplay : MonoBehaviour {
 	public ConvTree tree;
 	public string curPrompt;
+	public string outText;
 	public List<string> curResponses = new List<string>();
 	// Use this for initialization
 	void Awake(){
@@ -21,6 +22,8 @@ public class TextExtractAndDisplay : MonoBehaviour {
 	void Start () {
 		//TODO might need to change to describe as the starting node in the tree
 		//new variable or somesuch for the initial text
+
+		StartCoroutine("GetNewText");
 		curPrompt = tree.curNode.prompt;
 		for(int i = 0; i < tree.curNode.responses.Count; i++){
 			curResponses.Add(tree.curNode.responses[i]);
@@ -34,7 +37,24 @@ public class TextExtractAndDisplay : MonoBehaviour {
 	IEnumerator GetNewText(){
 
 		curPrompt = tree.curNode.prompt;
+		string newPrompt = NameReplace(curPrompt);
+
+		Debug.Log(newPrompt);
+		//curPrompt = PronounReplace(curPrompt);
 		yield return null;
+	}
+
+	//TODO put "characters" list in nodes and make sure that the node knows who is speaking at a given time for replacement
+	public string NameReplace(string inText){
+		if(inText.Contains("Well")){
+			return inText.Replace("Well", "bloop"); 
+		}
+		return null;
+	}
+	//TODO see above, but for pronouns
+	public string PronounReplace(string inText){
+
+		return null;
 	}
 
 }
