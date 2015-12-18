ce10ebe3eb0ed1c587975ac24dd762921a72e983
Hyacinth Nil <j.shepard.ramsay@gmail.com>
2015-12-18 00:20:37 -0500

    added onGui functions to deal with displaying text

---
+++ Assets/TextDisplay.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextDisplay : MonoBehaviour {
+
+    public TextMesh commandText;
+
+    void OnGUI()
+    {
+        Event e = Event.current;
+        if (e.type == EventType.keyDown)
+        {
+
+            AddText(e);
+            //RemoveText(e);
+            
+            
+            Debug.Log(commandText.text);
+
+        }
+        
+
+    }
+    void AddText(Event e)
+    {
+        if (e.keyCode != KeyCode.None)
+        {
+            if (e.keyCode != KeyCode.Space || e.keyCode != KeyCode.Backspace)
+            {
+                //get only alphabetical characters
+                commandText.text += e.keyCode;
+
+            }
+            else if (e.keyCode == KeyCode.Space)
+            {
+                commandText.text += " ";
+            }
+        }
+    }
+
+    void RemoveText(Event e)
+    {
+        if(e.keyCode == KeyCode.Backspace)
+        {
+            
+        }
+    }
+}
