512b899c10cc520e12bffc9eed0ae4cb2333f1ee
Azrael <j.shepard.ramsay@gmail.com>
2016-01-13 22:17:14 -0500

    fixed character generator

--- Assets/CharacterGenerator.cs
+++ Assets/CharacterGenerator.cs
@@ -4,22 +4,70 @@ using System.Collections.Generic;
 public class CharacterGenerator : MonoBehaviour {
 
     public Character playerCharacter;
-    public string[] characterIDs;
-    public string[] playableCharacterIDs;
+    //public string[] characterIDs;
+    //public string[] playableCharacterIDs;
+    public List<string> characterIDs = new List<string>();
+    public List<string> playableCharacterIDs = new List<string>();
+    public string[] genders;
     public string playerCharacterID;
 
     public Character playerCharacterPrefab;
     public Character nonPlayerCharacterPrefab;
-    // Use this for initialization
-    public Character CharacterInitialization() {
-        //Character newCharacter;
-        return nonPlayerCharacterPrefab;
+    void OnEnable() {
+        EventManager.StartListening("makePlayer", MakePlayer);
+        EventManager.StartListening("makeNonPlayers", MakeNonPlayers);
+    }
+
+    void OnDisable() {
+        EventManager.StopListening("makePlayer", MakePlayer);
+        EventManager.StopListening("makeNonPlayers", MakeNonPlayers);
+    }
+    public void CharacterInit() {
+        MakeNonPlayers();
+        
+    }
+   
+    void MakePlayer() {
+        
+        playerCharacterID = ChoosePlayer();
+        playerCharacter = Instantiate(playerCharacterPrefab, transform.position, transform.rotation) as Character;
+        playerCharacter.name = playerCharacterID;
+        GenerateGenders(playerCharacter);
+        playerCharacter.transform.parent = gameObject.transform;
+        for(int i = 0; i < characterIDs.Count; i++) {
+            if(characterIDs[i] == playerCharacterID) {
+                characterIDs.Remove(characterIDs[i]);
+
+            }
+
+        }
+
+        
+        
+    }
+
+    void MakeNonPlayers() {
+        
+        Character newCharacter;
+        for(int i = 0; i < characterIDs.Count; i++) {
+            newCharacter = Instantiate(nonPlayerCharacterPrefab, transform.position, transform.rotation) as Character;
+            newCharacter.name = characterIDs[i];
+            newCharacter.ID = characterIDs[i];
+            GenerateGenders(newCharacter);
+            newCharacter.transform.parent = gameObject.transform;
+        }
     }
 
 
 
     string ChoosePlayer() {
-        string playerID = playableCharacterIDs[Random.Range(0, playableCharacterIDs.Length)];
+        string playerID = playableCharacterIDs[Random.Range(0, playableCharacterIDs.Count)];
+        for(int i = 0; i < playableCharacterIDs.Count; i++) {
+            if(playableCharacterIDs[i] == playerID) {
+                playableCharacterIDs.Remove(playableCharacterIDs[i]);
+            }
+        }
+        //playableCharacterIDs
         return playerID;
     }
 
@@ -28,12 +76,12 @@ public class CharacterGenerator : MonoBehaviour {
 
     }
 
-    void GenerateNames() {
+    void GenerateNames(Character character) {
 
     }
 
-    void GenerateGenders() {
-
+    void GenerateGenders(Character character) {
+        character.Gender = genders[Random.Range(0, genders.Length)];
     }
 
 }
--- Assets/CharacterManager.cs
+++ Assets/CharacterManager.cs
@@ -2,17 +2,22 @@
 using System.Collections;
 using System.Collections.Generic;
 public class CharacterManager : MonoBehaviour {
-    CharacterGenerator generator;
-    List<Character> characters = new List<Character>();
+    
+    public List<Character> characters = new List<Character>();
     //database of characters and their during-play states
     //generated in character generator
-    void Start() {
-        generator = gameObject.GetComponent<CharacterGenerator>();
-        for(int i = 0; i < generator.characterIDs.Length; i++)
-        {
-            characters.Add(generator.CharacterInitialization());
-            Instantiate(characters[i]);
-            characters[i].transform.parent = gameObject.transform;
+    
+    void OnEnable() {
+        EventManager.StartListening("makeCharacters", AddCharactersToList);
+    }
+    void OnDisable() {
+        EventManager.StopListening("makeCharacters", AddCharactersToList);
+    }
+    void AddCharactersToList() {
+        EventManager.TriggerEvent("makePlayer");
+        EventManager.TriggerEvent("makeNonPlayers");
+        foreach (Transform children in transform) {
+            characters.Add(children.GetComponent<Character>());
         }
     }
 
