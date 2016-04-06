36b92dcb9af970468bc34f68d2d322c6c6f23d8e
Azrael <j.shepard.ramsay@gmail.com>
2016-04-06 12:09:47 -0400

    took out redundant code in effects manager

--- Assets/EffectManager.cs
+++ Assets/EffectManager.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class EffectManager : MonoBehaviour {
     Camera cameraMain;
-
+    postVHSPro vhs;
 	
 	
 	void OnEnable() {
@@ -21,34 +21,12 @@ public class EffectManager : MonoBehaviour {
 
     void SetupCamera() {
         cameraMain = Camera.main;
-        //Debug.Log(cameraMain);
+        vhs = GetComponent<postVHSPro>();
     }
 
     IEnumerator ScreenShake(int numOfShakes)
     {
-        //bool shake = true;
-        //float cameraPosX = Camera.main.transform.position.x;
-        //float newCameraPosX = Camera.main.transform.position.x;
-        /*while (shake == true)
-        {
-            Camera.main.orthographicSize = Random.Range(20, 23);
-            cameraMain.transform.position = new Vector3(Random.Range(-1, 1), 0, -10);
-            yield return new WaitForEndOfFrame();
-            Camera.main.orthographicSize = Random.Range(20, 23);
-            cameraMain.transform.position = new Vector3(Random.Range(-1, 1), 0, -10);
-            yield return new WaitForEndOfFrame();
-            Camera.main.orthographicSize = Random.Range(20, 23);
-            cameraMain.transform.position = new Vector3(Random.Range(-1, 1), 0, -10);
-            yield return new WaitForEndOfFrame();
-            Camera.main.orthographicSize = Random.Range(20, 23);
-            cameraMain.transform.position = new Vector3(Random.Range(-1, 1), 0, -10);
-            yield return new WaitForEndOfFrame();
-            Camera.main.orthographicSize = Random.Range(20, 23);
-            cameraMain.transform.position = new Vector3(Random.Range(-1, 1), 0, -10);
-            yield return new WaitForEndOfFrame();
-            shake = false;
-
-        }*/
+        
         for(int i = 0; i < numOfShakes; i++)
         {
             Camera.main.orthographicSize = Random.Range(20, 23);
@@ -57,8 +35,6 @@ public class EffectManager : MonoBehaviour {
         }        
         Camera.main.orthographicSize = 20;
         cameraMain.transform.position = new Vector3(0, 0, -10);
-
-        //newCameraPosX = cameraPosX;
 
     }
 }
