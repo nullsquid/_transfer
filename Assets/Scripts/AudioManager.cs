623d92be4263bef58d1a66db369b13d2b3abc612
Azrael <j.shepard.ramsay@gmail.com>
2016-05-03 11:37:31 -0400

    added play method to Audio Manager

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -3,7 +3,17 @@ using System.Collections;
 using System.Collections.Generic;
 public class AudioManager : Singleton<AudioManager> {
     
+    class ClipInfo {
+        public AudioSource source { get; set; }
+        public float defaultVolume { get; set; }
+
+    }
+
+    private List<ClipInfo> m_activeAudio;
+
     void Awake() {
+        m_activeAudio = new List<ClipInfo>();
+
         Debug.Log("Audiomanager initializing");
         try {
             transform.parent = GameObject.FindGameObjectWithTag("MainCamera").transform;
@@ -13,5 +23,22 @@ public class AudioManager : Singleton<AudioManager> {
             Debug.Log("Unable to find main camera");
         }
     }
+    public AudioSource Play(AudioClip clip, Vector3 soundOrigin, float volume) {
+        //Create an empty game object
+        GameObject soundLoc = new GameObject("Audio: " + clip.name);
+        soundLoc.transform.position = soundOrigin;
 
+        //Create the source
+        AudioSource source = soundLoc.AddComponent<AudioSource>();
+        SetSource(ref source, clip, volume);
+        source.Play();
+        Destroy(soundLoc, clip.length);
+
+        //Set the source as active
+        m_activeAudio.Add(new ClipInfo { source = source, defaultVolume = volume });
+        return source;
+    }
+    private void SetSource(ref AudioSource source, AudioClip clip, float volume) {
+
+    }
 }
