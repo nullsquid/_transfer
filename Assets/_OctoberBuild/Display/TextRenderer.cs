ab51fafbf6a5183c5222538ddd9e9327f89d71fe
DESKTOP-GF6OU0S\Azrael <j.shepard.ramsay@gmail.com>
2016-10-08 22:59:35 -0400

    changed font/surfaced variables to control puntuation pauses

--- Assets/_OctoberBuild/Display/TextRenderer.cs
+++ Assets/_OctoberBuild/Display/TextRenderer.cs
@@ -1,21 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using System.Collections.Generic;
 namespace TransferDisplay
 {
     public class TextRenderer : MonoBehaviour
     {
         #region Pulbic Variables
         //Descriptor variables
+        [HideInInspector]
         public string entityName;
 
         //Varibles that will change over time
+        [HideInInspector]
         public string entityText;
+        [HideInInspector]
         public string typewriterText;
 
         //System Variables
         public float timeBetweenLetters = .1f;
-        public int wordsPerLine;
+        public float punctuationTimeModifier;
+        public int maxLineLength;
+        public int maxChunkLength;
         public float xPadding = 100f;
         public float yPadding = 100f;
         public Font mainFont;
@@ -25,7 +30,9 @@ namespace TransferDisplay
 
         #region Private Variables
         GUIStyle style;
+
         private int wordsInLine;
+        private int linesInChunk;
         #endregion
 
         void Start()
@@ -46,20 +53,39 @@ namespace TransferDisplay
 
         IEnumerator IterateThroughCharactersToPrint(string text, float time)
         {
-
+            float normalTime = time;
                 for (int i = 0; i < text.Length; i++)
                 {
+                    if (text[i] == '.')
+                    {
+                        time += punctuationTimeModifier;
+                    }
+                    else if (text[i] == ',')
+                    {
+                        time += punctuationTimeModifier / 3;
+                    }
+                    else
+                    {
+                        time = normalTime;
+                    }
+
                     if(i > 0 && text[i - 1] == ' ')
                     {
                         
                         wordsInLine++;
                         
-                        if (wordsInLine > wordsPerLine)
+                        if (wordsInLine > maxLineLength)
                         {
-							
+                            linesInChunk++;
                             typewriterText += "\n";
                             typewriterText.Remove(i - 1, 1);
                             wordsInLine = 0;
+
+                            if(linesInChunk >= maxChunkLength)
+                            {
+                                //event to call the next chunk
+                                break;
+                            }
                         }
                 }
 					
