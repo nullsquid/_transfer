d4ed9cdca6dabbb59cc927bd5c93ea61b854e2ac
Hyacinth Nil <j.shepard.ramsay@gmail.com>
2016-10-25 12:13:39 -0400

    Added character database script and Character data object

---
+++ Assets/_OctoberBuild/Data/Collections/CharacterDatabase.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+namespace Transfer.Data
+{
+    public class CharacterDatabase
+    {
+        private static Dictionary<string, Character> characterDictionary = new Dictionary<string, Character>();
+
+        public 
+       
+    }
+}
---
+++ Assets/_OctoberBuild/System/Managers/CharacterManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using Transfer.Data;
+namespace Transfer.System
+{
+    public class CharacterManager : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
