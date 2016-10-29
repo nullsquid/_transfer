ba076271db0a0671704a1bff211d7283b37a64ae
Hyacinth Nil <j.shepard.ramsay@gmail.com>
2016-10-29 14:50:24 -0400

    converted char id arrays to lists

--- Assets/_OctoberBuild/System/Initialization/CharacterInitializer.cs
+++ Assets/_OctoberBuild/System/Initialization/CharacterInitializer.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using Transfer.Data;
 
 namespace Transfer.System
@@ -9,28 +10,50 @@ namespace Transfer.System
         
 
         public string playerID;
-        public string[] characterIDs = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "0" };
-        public string[] shortCharacterIDs = { "A", "E", "I", "0" };
-        string GeneratePlayerID(bool useShortCharacters)
-        {
-            
-            if (useShortCharacters)
-            {
-                playerID = shortCharacterIDs[Random.Range(0, shortCharacterIDs.Length)];
-            }
-            else
-            {
-                playerID = characterIDs[Random.Range(0, characterIDs.Length)];
-            }
-            return playerID;
-                
-        }
+        //public string[] characterIDs = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "0" };
+		public List<string> characterIDs = new List<string>();
+        //public string[] shortCharacterIDs = { "A", "E", "I", "0" };
+		public List<string> shortCharacterIDs = new List<string>();
+        
 
+		void AddCharacterIdentifiers(){
+			characterIDs.Add ("A");
+			characterIDs.Add ("B");
+			characterIDs.Add ("C");
+			characterIDs.Add ("D");
+			characterIDs.Add ("E");
+			characterIDs.Add ("F");
+			characterIDs.Add ("G");
+			characterIDs.Add ("H");
+			characterIDs.Add ("I");
+			characterIDs.Add ("0");
+			/////
+			/// 
+			///
+			shortCharacterIDs.Add("A");
+			shortCharacterIDs.Add("E");
+			shortCharacterIDs.Add("I");
+			shortCharacterIDs.Add("0");
+		}
 
-
+		string GeneratePlayerID(bool useShortCharacters)
+		{
+			if (useShortCharacters)
+			{
+				playerID = shortCharacterIDs[Random.Range(0, shortCharacterIDs.Count)];
+			}
+			else
+			{
+				playerID = characterIDs[Random.Range(0, characterIDs.Count)];
+			}
+			return playerID;
+		}
+		//make independent loops for both PC and NPC object lists
         Data.PlayerCharacter GeneratePlayerCharacter(string playerID, string playerName, Gender playerGender){
             Transfer.Data.PlayerCharacter newPlayer = new Transfer.Data.PlayerCharacter(playerID, playerName, playerGender);
+
             return newPlayer;
+
         }
 
         Data.NonPlayerCharacter GenerateCharacter(string charID, string charName, Gender charGender)
@@ -47,13 +70,16 @@ namespace Transfer.System
             return gender;
         }
 
+
+
 		public Data.Character GenerateCharacters(bool useShortCharacters)
         {
-			GeneratePlayerID (useShortCharacters);
+			AddCharacterIdentifiers ();
+			playerID = GeneratePlayerID (useShortCharacters);
 			Debug.Log ("player ID is " + playerID);
-			for(int i = 0; i < characterIDs.Length; i++)
+			for(int i = 0; i < characterIDs.Count; i++)
             {
-				string newID = characterIDs[Random.Range(0, characterIDs.Length)];
+				string newID = characterIDs[Random.Range(0, characterIDs.Count)];
 				Debug.Log ("new id is " + newID);
                 Data.Character newCharacter;
                 if (newID == playerID)
