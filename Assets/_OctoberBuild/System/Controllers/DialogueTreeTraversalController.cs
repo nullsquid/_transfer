01e7fdc3e66c8ad839f3caa0f6be34c4e856bd54
DESKTOP-GF6OU0S\Azrael <j.shepard.ramsay@gmail.com>
2016-11-10 13:13:54 -0500

    wrote skeleton for Node Traversal system

--- Assets/_OctoberBuild/System/Controllers/DialogueTreeTraversalController.cs
+++ Assets/_OctoberBuild/System/Controllers/DialogueTreeTraversalController.cs
@@ -8,7 +8,67 @@ namespace Transfer.System
     public class DialogueTreeTraversalController : MonoBehaviour
     {
         ConvTree curTree;
+        ConvNode curNode;
+        ///
+        #region Unity Callbacks
+        void OnEnable()
+        {
 
+        }
+        void OnDisable()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+        
+        void Start()
+        {
+            GetStartTree();
+        }
+        #endregion
+
+        #region Main Methods
+        void GetStartTree()
+        {
+            ConvTree startTree;
+            string startTreeName;
+            startTreeName = "9" + CharacterDatabase.GetPlayerID() + "_Tree";
+            foreach(Transform child in transform)
+            {
+                if(child.name == startTreeName)
+                {
+                    startTree = child.GetComponent<ConvTree>();
+                    Debug.Log(startTree + " " + startTreeName);
+                }
+            }
+        }
+
+        void GetStartNode()
+        {
+            curNode = curTree.GetComponent<Transform>().GetChild(0).GetComponent<ConvNode>();
+            Debug.Log(curNode);
+        }
+
+        public void TraverseToNextNode(int index)
+        {
+
+        }
+
+        public void GetSpeaker()
+        {
+            string speaker;
+            speaker = curNode.characterTalking;
+            CharacterDatabase.GetCharacterName(speaker);
+        }
+
+        
+
+
+        #endregion
 
     }
 }
