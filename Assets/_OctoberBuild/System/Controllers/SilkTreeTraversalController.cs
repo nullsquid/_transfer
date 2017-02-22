e290f06e3c37bff63ef142ef263c2fca2ac9d432
DESKTOP-GF6OU0S\Azrael <j.shepard.ramsay@gmail.com>
2017-02-21 20:51:59 -0500

    Added new class to traverse Silk stories

---
+++ Assets/_OctoberBuild/System/Controllers/SilkTreeTraversalController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using Silk;
+namespace Transfer.System
+{
+    public class SilkTreeTraversalController : MonoBehaviour
+    {
+
+        SilkMotherGraph _trees;
+
+        SilkGraph _startingTree;
+        SilkGraph _nextTree;
+        SilkGraph _curTree;
+
+        #region Unity Callbacks
+        
+        #endregion
+
+
+        #region Initialization Methods
+
+        #endregion
+
+
+        #region Main Methods
+
+        #endregion
+
+
+        #region Utility Methods
+
+        #endregion
+
+
+    }
+}
